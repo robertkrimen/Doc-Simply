3a7782859dad4b718a3a999ebc29288847e016f2
robertkrimen <robertkrimen@gmail.com>
2009-02-22 15:31:59 -0800

    Updated MANIFEST

--- lib/Doc/Simply.pm
+++ lib/Doc/Simply.pm
@@ -9,11 +9,11 @@ Doc::Simply - Generate POD-like documentation from embedded comments in JavaScri
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
